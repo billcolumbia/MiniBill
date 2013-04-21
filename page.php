5186246713042848ccefff33a867b143f3826990
AJ Foster <web@aj-foster.com>
2013-04-21 15:33:40 -0400

    Fix broken php tag in page file

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <div class="content-area">
 	<div class="main">
-		?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'content', 'page' ); ?>
 
