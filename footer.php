11c7e154e36a7bd5d4abf17bb118041a4f29abf1
Bill Columbia <hello@billcolumbia.com>
2013-04-21 13:02:36 -0400

    Update header and footer, and remove wp_head()

--- footer.php
+++ footer.php
@@ -7,6 +7,7 @@
 			<a href="#"></a>
 			<!-- Set the backgrounds of each anchor with logo image sprite so it's one HTTP request, use Sass -->
 		</footer>
+		<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
 <!-- FOOTER E -->
