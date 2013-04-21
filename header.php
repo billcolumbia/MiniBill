faaf9b90046f0d5ff039583d07d68edb54022c12
AJ Foster <web@aj-foster.com>
2013-04-21 17:38:00 -0400

    Fix search form in nav menu

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 		<![endif]-->
-		<link rel="stylesheet" type="text/css" href="styles.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 	</head>
 	<body class="PAGE-SLUG-HERE">
 		<div class="page">
