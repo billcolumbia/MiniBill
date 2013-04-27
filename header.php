ac6ad3556b0d46922f2fbeb08066e62f5139da1e
AJ Foster <web@aj-foster.com>
2013-04-26 21:21:19 -0400

    Add body class and custom navigation.

--- header.php
+++ header.php
@@ -10,7 +10,16 @@
 		<![endif]-->
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 	</head>
-	<body class="PAGE-SLUG-HERE">
+<?php
+	global $post;
+	if ($post->post_type == "page")
+		$body_class = 'class="page-' . $post->post_name . '"';
+	else if ($post->post_type != "")
+		$body_class = ($post->post_type != 'post') ? 'class="post-' . $post->post_type . '"' : 'class="post-default"';
+	else
+		$body_class = "";
+?>
+	<body <?php echo $body_class; ?>>
 		<div class="page">
 			<header>
 				<nav class="main-menu full">
@@ -19,8 +28,26 @@
 						<a class="menu-toggle">Menu</a>
 						<?php get_search_form(); ?>
 					</div>
-					<?php wp_nav_menu(array('theme_location' => 'primary', 'container' => false)); ?>
-					
+					<ul>
+<?php
+							$current_ID = $post->ID;
+
+							$navQuery = array('post_type' => 'page', 'post_status' => 'publish', 'posts_per_page' => -1, 'orderby' => 'menu_order', 'order' => 'ASC');
+							$navLoop = new WP_Query($navQuery);
+
+							while ($navLoop->have_posts()) {
+
+								$navLoop->the_post();
+
+								$name = get_the_title();
+								$link = get_permalink();
+								$nav_li_class = (get_the_ID() == $current_ID) ? ' class="current" ' : '';
+
+							
+?>
+						<li<?php echo $nav_li_class; ?>><a href="<?php echo $link; ?>"><?php echo $name; ?></a></li>
+<?php 						} ?>
+					</ul>
 				</nav>
 			</header>
 <!-- HEADER END -->
