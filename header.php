eca5a83476b8e4f7c4b77e4310b1c1b922d4c7f0
AJ Foster <web@aj-foster.com>
2013-05-15 13:29:18 -0400

    Add Google Chrome Frame support.

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width" />
+		<meta http-equiv="X-UA-Compatible" content="chrome=1" />
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
