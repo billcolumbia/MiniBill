fbde11430f30b105df955a77431fc9c08d4c48ad
AJ Foster <web@aj-foster.com>
2013-07-15 20:44:33 -0400

    Remove some admin pages for all users.  Update comments.

--- functions.php
+++ functions.php
@@ -25,22 +25,26 @@ add_action('after_setup_theme', 'setup_thumbnails');
  */
 
 function remove_menus() {
+
+	/* Pages removed for all users, including administrators. */
+
+	remove_menu_page('edit.php');
+		remove_submenu_page('edit.php', 'post-new.php');
+		remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=category');
+		remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=post_tag');
+	remove_menu_page('upload.php');
+		remove_submenu_page('upload.php', 'media-new.php');
+	remove_menu_page('link-manager.php');
+		remove_submenu_page('link-manager.php', 'link-add.php');
+		remove_submenu_page('link-manager.php', 'edit-tags.php?taxonomy=link_category');
+	remove_menu_page('edit-comments.php');
+
 	$user = wp_get_current_user();
 	if ($user->wp_capabilities['administrator'] != 1) {
 
 			remove_submenu_page('index.php', 'update-core.php');
-		remove_menu_page('edit.php');
-			remove_submenu_page('edit.php', 'post-new.php');
-			remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=category');
-			remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=post_tag');
-		remove_menu_page('upload.php');
-			remove_submenu_page('upload.php', 'media-new.php');
-		remove_menu_page('link-manager.php');
-			remove_submenu_page('link-manager.php', 'link-add.php');
-			remove_submenu_page('link-manager.php', 'edit-tags.php?taxonomy=link_category');
 		remove_menu_page('edit.php?post_type=page');
 			remove_submenu_page('edit.php', 'post-new.php?post_type=page');
-		remove_menu_page('edit-comments.php');
 		remove_menu_page('themes.php');
 			remove_submenu_page('themes.php', 'widgets.php');
 			remove_submenu_page('themes.php', 'nav-menus.php');
@@ -48,9 +52,9 @@ function remove_menus() {
 		remove_menu_page('plugins.php');
 			remove_submenu_page('plugins.php', 'plugin-install.php');
 			remove_submenu_page('plugins.php', 'plugin-editor.php');
-		remove_menu_page('users.php');
+		//remove_menu_page('users.php');
 			remove_submenu_page('users.php', 'user-new.php');
-			remove_submenu_page('users.php', 'profile.php');
+			//remove_submenu_page('users.php', 'profile.php');
 		remove_menu_page('tools.php');
 			remove_submenu_page('tools.php', 'import.php');
 			remove_submenu_page('tools.php', 'export.php');
@@ -116,6 +120,8 @@ function cpt_icons() {
 }
 //add_action('admin_head', 'cpt_icons');
 
+
+/* To include other collections of functions, include_once() the relevant files here. */
 
 include_once("functions/functions-nav.php");
 
